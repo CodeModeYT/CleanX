fe2706578151f15942d2dfe47ce3a056e5b68b90
CodeModeYT <tillmann.menzer@outlook.com>
2024-08-21 21:04:32 +0200

    react-native project setup

---
+++ windows/cleanX/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::cleanX::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::cleanX::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
